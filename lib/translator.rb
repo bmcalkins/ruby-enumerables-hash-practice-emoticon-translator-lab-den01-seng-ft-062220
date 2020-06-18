2a5d1ad3f9b50194ec9dccfeb529c22b361f94c3
Bradley Calkins <b.raders1013@gmail.com>
2020-06-18 03:33:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
 # require modules here
-
-def load_library
+require "yaml"
+def load_library(filename)
+  raw_hash =  YAML.load(File.read(filename))
+  hash = Hash.new
+  raw_hash.each do |key, value|
+    
+    hash[key]= {'english': value[0], 'japanese': value[1]}
+    
+  end
+  
+  return hash
   # code goes here
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(filename, emoticon)
   # code goes here
+  hash = load_library(filename)
+  return hash[emoticon]['japanese']
 end
 
-def get_english_meaning
+def get_english_meaning(filename, emoticon)
+  hash = load_library(filename)
+  hash.each do |key, value|
+    puts(hash[key]['japanese'])
+    if value['japanese'] == emoticon
+      return key
+    end
+  end
+  return 'sorry, emoticon not found'
   # code goes here
 end
